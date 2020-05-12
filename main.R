a858de748a903300f03208d1391a503aee4ee648
akbasberkay <akbasberkay@hotmail.com>
2020-05-12 15:24:23 +0300

    Update main.R
    added naive forecast pipeline

--- main.R
+++ main.R
@@ -1,1 +1,12 @@
 source("api_connection.R")
+
+# Requirements
+require("forecast")
+require("ggplot2")
+
+# Naive forecast pipeline
+pipeline_naive = function(ts){
+  ts %>% naive() %>% checkresiduals()
+  readline(prompt = "Please press a key to continue")
+  print("OK!")
+}
