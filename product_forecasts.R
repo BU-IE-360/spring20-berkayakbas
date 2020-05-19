9cd514c8974c23d59c7bf2903b5d2ba7e183f67a
akbasberkay <akbasberkay@hotmail.com>
2020-05-19 19:45:37 +0300

    variable name changes

--- product_forecasts.R
+++ product_forecasts.R
@@ -12,21 +12,21 @@ method_names = c(
   'Stepwise Forward'
 )
 forecast <- rep(NA, length(method_names))
-accuracy_ADJ_R2<- rep(NA, length(method_names))
-accuracy_ME <- rep(NA, length(method_names))
-accuracy_RMSE <- rep(NA, length(method_names))
-accuracy_MAE <- rep(NA, length(method_names))
-accuracy_MAPE <- rep(NA, length(method_names))
-accuracy_MASE <- rep(NA, length(method_names))
-accuracy_ACF1 <- rep(NA, length(method_names))
+ADJ_R2<- rep(NA, length(method_names))
+ME <- rep(NA, length(method_names))
+RMSE <- rep(NA, length(method_names))
+MAE <- rep(NA, length(method_names))
+MAPE <- rep(NA, length(method_names))
+MASE <- rep(NA, length(method_names))
+ACF1 <- rep(NA, length(method_names))
 
 #set test metrics
-accuracy_test_ADJ_R2<- rep(NA, length(method_names))
-accuracy_test_ME <- rep(NA, length(method_names))
-accuracy_test_RMSE <- rep(NA, length(method_names))
-accuracy_test_MAE <- rep(NA, length(method_names))
-accuracy_test_MAPE <- rep(NA, length(method_names))
-accuracy_test_MASE <- rep(NA, length(method_names))
+test_ADJ_R2<- rep(NA, length(method_names))
+test_ME <- rep(NA, length(method_names))
+test_RMSE <- rep(NA, length(method_names))
+test_MAE <- rep(NA, length(method_names))
+test_MAPE <- rep(NA, length(method_names))
+test_MASE <- rep(NA, length(method_names))
 
 
 #get train and test subsets of data
@@ -46,21 +46,21 @@ index = 1
 model = naive(product_data_xts$sold_count, h = 2)
 forecast[index] <- as.numeric(model$mean[2])
 accuracy = accuracy(model)
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- accuracy[, 'ACF1']
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- accuracy[, 'ACF1']
 
 #1.1 Naive Testing
 model=naive(train_data_xts$sold_count, h = nrow(test_data_xts))
 accuracy=accuracy(model, test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 
 preds_1<- xts(model$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -71,21 +71,21 @@ index = 2
 model = meanf(product_data_xts$sold_count, h = 2)
 forecast[index] <- as.numeric(model$mean[2])
 accuracy = accuracy(model)
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- NA
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- NA
 
 #2.1. Mean Testing
 model=meanf(train_data_xts$sold_count, h = nrow(test_data_xts))
 accuracy=accuracy(model, test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 
 preds_1<- xts(model$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -96,22 +96,22 @@ index = 3
 model = HoltWinters(ts(product_data_xts$sold_count , frequency = 7)[, ], seasonal = "additive")
 forecast[index] <-as.numeric(forecast(model, h = 2)$mean[2])
 accuracy = accuracy(forecast(model, h=2))
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- accuracy[, 'ACF1']
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- accuracy[, 'ACF1']
 
 #3.1 HW Add Testing
 
 model=HoltWinters(ts(train_data_xts$sold_count , frequency = 7)[, ], seasonal = "additive")
 accuracy = accuracy(forecast(model, h=nrow(test_data_xts)), test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 
 preds_1<- xts(forecast(model, h=nrow(test_data_xts))$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -124,21 +124,21 @@ if (sum(ts(product_data_xts$sold_count , frequency = 7)[, 1] == 0) == 0) {
   model = HoltWinters(ts(product_data_xts$sold_count , frequency = 7)[, ], seasonal = "multiplicative")
   forecast[index] <-as.numeric(forecast(model, h = 2)$mean[2])
   accuracy = accuracy(forecast(model, h=2))
-  accuracy_ME[index] <- accuracy[, 'ME']
-  accuracy_RMSE[index] <- accuracy[, 'RMSE']
-  accuracy_MAE[index] <- accuracy[, 'MAE']
-  accuracy_MAPE[index] <- accuracy[, 'MAPE']
-  accuracy_MASE[index] <- accuracy[, 'MASE']
-  accuracy_ACF1[index] <- accuracy[, 'ACF1']
+  ME[index] <- accuracy[, 'ME']
+  RMSE[index] <- accuracy[, 'RMSE']
+  MAE[index] <- accuracy[, 'MAE']
+  MAPE[index] <- accuracy[, 'MAPE']
+  MASE[index] <- accuracy[, 'MASE']
+  ACF1[index] <- accuracy[, 'ACF1']
   
   #HW Mult testing
   model=HoltWinters(ts(train_data_xts$sold_count , frequency = 7)[, ], seasonal = "multiplicative")
   accuracy = accuracy(forecast(model, h=nrow(test_data_xts)), test_data_xts$sold_count)
-  accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-  accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-  accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-  accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-  accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+  test_ME[index] <- accuracy["Test set", 'ME']
+  test_RMSE[index] <- accuracy["Test set", 'RMSE']
+  test_MAE[index] <- accuracy["Test set", 'MAE']
+  test_MAPE[index] <- accuracy["Test set", 'MAPE']
+  test_MASE[index] <- accuracy["Test set", 'MASE']
   preds_1<- xts(forecast(model, h=nrow(test_data_xts))$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
   print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
   lines(preds_1, col = "red")
@@ -151,22 +151,22 @@ index = 5
 model = ses(product_data_xts$sold_count, h = 2)
 forecast[index] <- as.numeric(forecast(model, h = 2)$mean[2])
 accuracy = accuracy(model)
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- accuracy[, 'ACF1']
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- accuracy[, 'ACF1']
 
 #5.1 Exp Smt Testing
 
 model=ses(train_data_xts$sold_count, h = nrow(test_data_xts))
 accuracy=accuracy(model, test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 
 preds_1<- xts(model$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -177,22 +177,22 @@ index = 6
 model = auto.arima(product_data_xts$sold_count)
 forecast[index] <- as.numeric(forecast(model, h = 2)$mean[2])
 accuracy = accuracy(model)
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- accuracy[, 'ACF1']
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- accuracy[, 'ACF1']
 
 # 6.1 Auto Arima Testing
 
 model=auto.arima(train_data_xts$sold_count)
 accuracy=accuracy(forecast(model, h = nrow(test_data_xts)), test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 preds_1<- xts(forecast(model, h = nrow(test_data_xts))$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
 lines(preds_1, col = "red")
@@ -201,22 +201,22 @@ index = 7
 model = tbats(ts(product_data_xts$sold_count))
 forecast[index] <- as.numeric(forecast(model, h = 2)$mean[2])
 accuracy = accuracy(model)
-accuracy_ME[index] <- accuracy[, 'ME']
-accuracy_RMSE[index] <- accuracy[, 'RMSE']
-accuracy_MAE[index] <- accuracy[, 'MAE']
-accuracy_MAPE[index] <- accuracy[, 'MAPE']
-accuracy_MASE[index] <- accuracy[, 'MASE']
-accuracy_ACF1[index] <- accuracy[, 'ACF1']
+ME[index] <- accuracy[, 'ME']
+RMSE[index] <- accuracy[, 'RMSE']
+MAE[index] <- accuracy[, 'MAE']
+MAPE[index] <- accuracy[, 'MAPE']
+MASE[index] <- accuracy[, 'MASE']
+ACF1[index] <- accuracy[, 'ACF1']
 
 #7.1 TBATS Testing
 
 model=tbats(ts(train_data_xts$sold_count))
 accuracy=accuracy(forecast(model, h = nrow(test_data_xts)), test_data_xts$sold_count)
-accuracy_test_ME[index] <- accuracy["Test set", 'ME']
-accuracy_test_RMSE[index] <- accuracy["Test set", 'RMSE']
-accuracy_test_MAE[index] <- accuracy["Test set", 'MAE']
-accuracy_test_MAPE[index] <- accuracy["Test set", 'MAPE']
-accuracy_test_MASE[index] <- accuracy["Test set", 'MASE']
+test_ME[index] <- accuracy["Test set", 'ME']
+test_RMSE[index] <- accuracy["Test set", 'RMSE']
+test_MAE[index] <- accuracy["Test set", 'MAE']
+test_MAPE[index] <- accuracy["Test set", 'MAPE']
+test_MASE[index] <- accuracy["Test set", 'MASE']
 preds_1<- xts(forecast(model, h = nrow(test_data_xts))$mean, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
 lines(preds_1, col = "red")
@@ -228,18 +228,18 @@ newdata_f<-product_data_xts[(nrow(product_data_xts)-1):nrow(product_data_xts), c
 index(newdata_f)<-(index(newdata_f)+2)
 preds<-predict(lm_model_1, newdata = newdata_f)
 forecast[index] <- preds[2]
-accuracy_ADJ_R2[index]<-summary(lm_model_1)$adj.r.squared
-accuracy_MAE[index] <-MAE(lm_model_1$fitted.values,product_data_xts$sold_count)
-accuracy_RMSE[index] <- sqrt(MSE(lm_model_1$fitted.values,product_data_xts$sold_count))
-accuracy_MAPE[index] <- mape(product_data_xts$sold_count, lm_model_1$fitted.values)
+ADJ_R2[index]<-summary(lm_model_1)$adj.r.squared
+MAE[index] <-MAE(lm_model_1$fitted.values,product_data_xts$sold_count)
+RMSE[index] <- sqrt(MSE(lm_model_1$fitted.values,product_data_xts$sold_count))
+MAPE[index] <- MAPE(product_data_xts$sold_count, lm_model_1$fitted.values)
 
 # 8.1 Linear Regression Testing
 lm_model_1<-lm(sold_count~ visit_count+favored_count+basket_count, data=train_data_xts)
 preds<-predict(lm_model_1, newdata = testing_for_newdata_xts)
-accuracy_test_ADJ_R2[index]<-summary(lm_model_1)$adj.r.squared
-accuracy_test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
-accuracy_test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
-accuracy_test_MAPE[index] <- mape(test_data_xts$sold_count, preds)
+test_ADJ_R2[index]<-summary(lm_model_1)$adj.r.squared
+test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
+test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
+test_MAPE[index] <- MAPE(test_data_xts$sold_count, preds)
 
 preds_1<- xts(preds, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -262,17 +262,17 @@ newdata_f<-product_data_xts[(nrow(product_data_xts)-1):nrow(product_data_xts),]
 index(newdata_f)<-(index(newdata_f)+2)
 preds<-predict(backward_lr, newdata = newdata_f)
 forecast[index] <- preds[2]
-accuracy_ADJ_R2[index]<-summary(backward_lr)$adj.r.squared
-accuracy_MAE[index] <-MAE(backward_lr$fitted.values,product_data_xts$sold_count)
-accuracy_RMSE[index] <- sqrt(MSE(backward_lr$fitted.values,product_data_xts$sold_count))
-accuracy_MAPE[index] <- mape(product_data_xts$sold_count, backward_lr$fitted.values)
+ADJ_R2[index]<-summary(backward_lr)$adj.r.squared
+MAE[index] <-MAE(backward_lr$fitted.values,product_data_xts$sold_count)
+RMSE[index] <- sqrt(MSE(backward_lr$fitted.values,product_data_xts$sold_count))
+MAPE[index] <- MAPE(product_data_xts$sold_count, backward_lr$fitted.values)
 
 #9.1 Step Backward -Test
 preds<-predict(backward_lr_test, newdata = testing_for_newdata_xts)
-accuracy_test_ADJ_R2[index]<-summary(backward_lr_test)$adj.r.squared
-accuracy_test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
-accuracy_test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
-accuracy_test_MAPE[index] <- mape(test_data_xts$sold_count, preds)
+test_ADJ_R2[index]<-summary(backward_lr_test)$adj.r.squared
+test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
+test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
+test_MAPE[index] <- MAPE(test_data_xts$sold_count, preds)
 
 preds_1<- xts(preds, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
@@ -283,25 +283,26 @@ newdata_f<-product_data_xts[(nrow(product_data_xts)-1):nrow(product_data_xts),]
 index(newdata_f)<-(index(newdata_f)+2)
 preds<-predict(forward_lr, newdata = newdata_f)
 forecast[index] <- preds[2]
-accuracy_ADJ_R2[index]<-summary(forward_lr)$adj.r.squared
-accuracy_MAE[index] <-MAE(forward_lr$fitted.values,product_data_xts$sold_count)
-accuracy_RMSE[index] <- sqrt(MSE(forward_lr$fitted.values,product_data_xts$sold_count))
-accuracy_MAPE[index] <- mape(product_data_xts$sold_count, forward_lr$fitted.values)
+ADJ_R2[index]<-summary(forward_lr)$adj.r.squared
+MAE[index] <-MAE(forward_lr$fitted.values,product_data_xts$sold_count)
+RMSE[index] <- sqrt(MSE(forward_lr$fitted.values,product_data_xts$sold_count))
+MAPE[index] <- MAPE(product_data_xts$sold_count, forward_lr$fitted.values)
 
 #10.1 Step Forward Test
 preds<-predict(forward_lr_test, newdata = testing_for_newdata_xts)
-accuracy_test_ADJ_R2[index]<-summary(forward_lr_test)$adj.r.squared
-accuracy_test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
-accuracy_test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
-accuracy_test_MAPE[index] <- mape(test_data_xts$sold_count, preds)
+test_ADJ_R2[index]<-summary(forward_lr_test)$adj.r.squared
+test_MAE[index] <-MAE(preds,test_data_xts$sold_count)
+test_RMSE[index] <- sqrt(MSE(preds,test_data_xts$sold_count))
+test_MAPE[index] <- MAPE(test_data_xts$sold_count, preds)
 
 preds_1<- xts(preds, order.by= seq(first(index(test_data_xts)),last(index(test_data_xts)), by="days"))
 print(plot(c(train_data_xts$sold_count,test_data_xts$sold_count), main = paste0(method_names[index])))
 lines(preds_1, col = "red")
 
-columns = cbind(forecast, accuracy_ADJ_R2, accuracy_ME, accuracy_RMSE, accuracy_MAE, accuracy_MAPE, accuracy_MASE, 
-                accuracy_ACF1,accuracy_test_ADJ_R2, accuracy_test_ME, accuracy_test_RMSE,
-                accuracy_test_MAE,accuracy_test_MAPE,accuracy_test_MASE)
+columns = cbind(forecast, ADJ_R2,MAE, MAPE, MASE, test_ADJ_R2, test_MAE, test_MAPE, test_MASE)
+# columns = cbind(forecast, ADJ_R2, ME, RMSE, MAE, MAPE, MASE, 
+#                 ACF1,test_ADJ_R2, test_ME, test_RMSE,
+#                 test_MAE,test_MAPE,test_MASE)
 results <- as.data.frame(columns)
 row.names(results) <- method_names
 print("Product Forecasts Done!")
