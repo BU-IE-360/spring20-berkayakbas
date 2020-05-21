42d62b99e92566e35c87f5d4271476ad65b91f46
akbasberkay <akbasberkay@hotmail.com>
2020-05-22 02:59:00 +0300

    small changes
    product_name variable created

--- main3.R
+++ main3.R
@@ -16,8 +16,9 @@ source("make_xts.R")
 product_data_xts=tail(product_data_xts,filter_last_n_days)
 print(paste0("Last event date: ", tail(data_dt, 1)$event_date))
 print(paste0("Product ID: ", product_id))
-print(paste0("Product Name: ", product_names_dt[product_content_id == product_id]$bottom_hierarchy))
-browseURL(toString(product_names_dt[product_content_id == product_id]$url))
+product_name = product_names_dt[product_content_id == product_id]$bottom_hierarchy
+print(paste0("Product Name: ", product_name))
+# browseURL(toString(product_names_dt[product_content_id == product_id]$url))
 source("product_forecasts.R")
 source("rolling_origin.R")
 
