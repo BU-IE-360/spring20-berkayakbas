e2a7d1df6f4c4f34227d168c47f7391b90990e04
akbasberkay <akbasberkay@hotmail.com>
2020-05-23 19:47:36 +0300

    results are ordered by ro_MAPE

--- main3.R
+++ main3.R
@@ -11,6 +11,10 @@ product_names_dt = data.table(read.csv("API Products.csv", sep = ';'))
 product_ids = unique(data_dt$product_content_id)
 product_id = product_ids[forecasted_product_number]
 product_data_dt = data_dt[product_content_id == product_id]
+# Fix tayt data for stock-out day
+if (product_id == 31515569){
+  product_data_dt[event_date == as.Date('2020-05-17'),] = product_data_dt[event_date == as.Date('2020-05-16'),]
+}
 source("make_xts.R")
 
 product_data_xts=tail(product_data_xts,filter_last_n_days)
@@ -18,17 +22,21 @@ print(paste0("Last event date: ", tail(data_dt, 1)$event_date))
 print(paste0("Product ID: ", product_id))
 product_name = product_names_dt[product_content_id == product_id]$bottom_hierarchy
 print(paste0("Product Name: ", product_name))
-# browseURL(toString(product_names_dt[product_content_id == product_id]$url))
+browseURL(toString(product_names_dt[product_content_id == product_id]$url))
 source("product_forecasts.R")
 source("rolling_origin.R")
 
+results = results[order(results$ro_MAPE),]
 View(results)
 
 # Initialize Predictions
-predictions=unique(data_dt[,list(product_content_id)])
-predictions[,forecast:=0]
+if (exists("predictions")){
+  predictions=unique(data_dt[,list(product_content_id)])
+  predictions[,forecast:=0]
+  print("Predictions are initialized")
+}
 # Set Product Prediction
-predictions[product_content_id == product_id]$forecast = 85
+predictions[product_content_id == product_id]$forecast = 1
 View(predictions)
 
 # Send Submission
