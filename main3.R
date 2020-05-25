cdce60a832970666d4dcba587d705fe093767d0d
akbasberkay <akbasberkay@hotmail.com>
2020-05-25 13:40:21 +0300

    removed unnecessary initialization

--- main3.R
+++ main3.R
@@ -6,10 +6,8 @@ source("inputs.R")
 source("product_analysis.R")
 View(results)
 
-# Initialize Predictions
-predictions = initialize_predictions()
 # Set Product Prediction
-predictions = set_prediction(product_id = product_id, prediction = 5)
+predictions = set_prediction(product_id = product_id, prediction = 1)
 
 # Send Submission
 # send_submission(predictions, token, url=subm_url, submit_now=F)
